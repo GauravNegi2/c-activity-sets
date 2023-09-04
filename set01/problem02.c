e8f4e12776ab3ddb439c4e55769c1e6818fd5551
GauravNegi2 <142887005+GauravNegi2@users.noreply.github.com>
2023-09-04 16:00:05 +0530

    GauravNegiproblem02.c

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,11 @@
 //2. Write a C program to add two numbers.
-
+#include<stdio.h>
+int main()
+{
+    int x=2,y=3,sum=0;
+    {
+        sum=x+y;
+        printf("%d+%d=%d",x,y,sum);
+        return 0;
+    }
+}
